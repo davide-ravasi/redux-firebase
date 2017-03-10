f9191999f7f8c39a21596beacbeb782f27a79560
davide-ravasi <info@davideravasi.com>
2017-03-10 17:23:58 +0100

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PostList from './post_list';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+      	<h1>React simple starter</h1>
+      	<PostList />
+
+      </div>
+    );
+  }
+}
